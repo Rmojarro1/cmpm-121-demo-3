994b61cf3a8039fdd509d47d9d0df4e8efaab7e2
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-11-04 12:19:32 -0800

    Created basic button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 // todo
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameName = "Raul's D3";
+
+document.title = gameName;
+//document.body.style.backgroundColor = "purple";
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+
+const button = document.createElement("button");
+app.appendChild(header);
+app.appendChild(button);
+button.innerHTML = "Click me";
+
+button.onclick = () => {
+  alert("The button has been clicked");
+};
